fa1262d97870f20a5dcef3ef3f62910486cb60ea
Martin Picollo <martin.picollo@wolox.com.ar>
2020-02-18 19:32:07 -0300

    Packages WIP

--- test/tools_schemas.spec.js
+++ test/tools_schemas.spec.js
@@ -1,7 +1,14 @@
-'use strict';
+const { matchers } = require('jest-json-schema');
+
+const tools = require('../generators/app/tools.json');
+const expectedToolSchema = require('./utils/schemas/tool_schema.json');
+
+expect.extend(matchers);
+
+const toolsOptions = Object.keys(tools).map(toolName => [toolName, tools[toolName]]);
 
 describe('generic tool schemas', () => {
-  it('creates files', () => {
-    // assert.file(['dummyfile.txt']);
+  it.each(toolsOptions)('should match schema for tool: %s', (_, toolSchema) => {
+    expect(toolSchema).toMatchSchema(expectedToolSchema);
   });
 });
